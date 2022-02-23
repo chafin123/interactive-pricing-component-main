7005c9525d363f66102488a8e47737d1ca70f82a
chafin123 <stevend.chafin@gmail.com>
2022-02-23 17:55:12 -0600

    added slider, range, button, logic works

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
-// const slider = document.getElementById("sliderRange");
-// const output = document.getElementById("rangeNumbers");
-// output.innerHTML = slider.value; // Display the default slider value
+// Changes pageviews to correct number in accordance with range slider value
 
-// // Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
-// }
+const slider = document.getElementById("pageViewSlider");
+const output = document.getElementById("pageViewNumber");
+
+function convertRangeValueIntoPageViews(range) {
+    switch(range.value) {
+        case '0':
+            return '10K';
+            break;
+        case '25':
+            return '50K';
+            break;
+        case '50':
+            return '100K';
+            break;
+        case '75':
+            return '500K';
+            break;
+        case '100':
+            return '1M';
+            break;
+    }   
+}
+function pageViews() {
+    return convertRangeValueIntoPageViews(slider)
+}
+function monthlyPrice() {
+    return getMonthyBillBasedOnPageViews()
+}
+slider.onchange = function() {
+    output.innerHTML = pageViews();
+    monthlyCost.innerHTML = monthlyPrice();
+}
+const sliderButton = document.querySelector('input[name="discountSlider"]');
+const monthlyCost = document.getElementById('rangeNumbers');
+//change monthy price based on range slider and if discount applyied
+function getMonthyBillBasedOnPageViews() {
+    let pageViewNumbers = pageViews();
+    switch(pageViewNumbers) {
+        case '10K':
+            if(sliderButton.checked == true) {
+                return "$6.00"
+            } return "$8.00"
+            break;
+        case '50K':
+            if(sliderButton.checked == true) {
+                return "$9.00"
+            } return "$12.00"
+            break;
+        case '100K':
+            if(sliderButton.checked == true) {
+                return "$12.00"
+            } return "$16.00"
+            break;
+        case '500K':
+            if(sliderButton.checked == true) {
+                return "$18.00"
+            } return "$24.00"
+            break;
+        case '1M':
+            if(sliderButton.checked == true) {
+                return "$27.00"
+            } return "$36.00"
+            break;
+    }
+}
+sliderButton.onchange = function() {
+    monthlyCost.innerHTML = monthlyPrice();
+}
+// JS for button slider
 const range = document.querySelector('.range')
 const thumb = document.querySelector('.thumb')
 const track = document.querySelector('.track-inner')
